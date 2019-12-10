082c2b691597e2949daaacd9fb3e956e0945930d
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-12-10 17:04:21 +0200

    INFRA-10106 | Add library assembling to makefile

--- X2tConverter/X2tWorker.mk
+++ X2tConverter/X2tWorker.mk
@@ -38,25 +38,69 @@ White      = \033[1;37m
 Gray       = \033[1;90m
 
 # Current Working Dir (Full path)
-CWD    := $(shell cd $(shell dirname $(THIS_MAKEFILE)); pwd)
+CWD       := $(shell cd $(shell dirname $(THIS_MAKEFILE)); pwd)
 # Filter Makefile Input params to use they as target input params
-ARGS   := $(filter-out $@, $(MAKECMDGOALS))
+ARGS      := $(filter-out $@, $(MAKECMDGOALS))
+
+# =================================================================
+# Target filters:
+# =================================================================
+BUILD_ARGS := $(filter-out build, $(MAKECMDGOALS))
+CLEAN_ARGS := $(filter-out clean, $(MAKECMDGOALS))
+PLATFORM  = $(if $(BUILD_ARGS),$(BUILD_ARGS), "")
+
+# =================================================================
+# Project definitions:
+# =================================================================
+# X2t converter build dir for assemble output
+BUILD_DIR := $(CWD)/build
+
+CORE_BUILD_DIR := $(abspath $(CWD)/../build)
+
+ifeq ($(shell uname -m),x86_64)
+	ARCH := 64
+else
+	ARCH := 32
+endif
+
+TARGET_BUILD := $(PLATFORM)_$(ARCH)
+
+ifeq ($(PLATFORM),mac)
+	SHARED_EXT := .dylib
+	LIB_EXT := .a
+	LIB_PREFIX := lib
+else
+	ifeq ($(PLATFORM),linux)
+		SHARED_EXT := .so*
+		LIB_EXT := .a
+		LIB_PREFIX := lib
+	endif
+endif
+
 
 # =================================================================
 # Makefile Targets:
 # =================================================================
 
 ---: ## --------------------------------------------------------------
-build: ## Assemble x2t converter from build artifacts
-	echo "Assemble"
+build: ## Assemble x2t converter from Core build artifacts
+	echo "$(Cyan)Assemble x2t converter for $(PLATFORM)$(NC)"
+
+	[ -d $(BUILD_DIR)/$(TARGET_BUILD) ] || mkdir -p $(BUILD_DIR)/$(TARGET_BUILD)
+	cp $(CORE_BUILD_DIR)/lib/$(TARGET_BUILD)/*$(SHARED_EXT) $(BUILD_DIR)/$(TARGET_BUILD)
+
+clean: ## Cleanup x2t converter assemblies
+	[ -d $(BUILD_DIR)/$(TARGET_BUILD)) ] && (rm -rf $(BUILD_DIR)/$(TARGET_BUILD)) && echo "Deleted $(BUILD_DIR)/$(TARGET_BUILD))")
+	exit 0
 
 ---: ## --------------------------------------------------------------
 help: .logo ## Show this help and exit
-	@echo "$(Yellow)Usage:$(NC)\n  make -f $(THIS_MAKEFILE) <target> <arguments...>"
+	@echo "$(Yellow)Usage:$(NC)\n  make -f $(THIS_MAKEFILE) <target> <target_os>"
 	@echo ''
-	# @echo "$(Yellow)Arguments:$(NC)"
-	# printf "  $(Green)%-15s$(NC) %s\n" "testName" "for all test runners - Filter which tests to run"
-	# @echo ''
+	@echo "$(Yellow)Target OS:$(NC)"
+	printf "  $(Green)%-15s$(NC) %s\n" "mac" "Assemble x2t converter for MacOs"
+	printf "  $(Green)%-15s$(NC) %s\n" "linux" "Assemble x2t converter for Linux"
+	@echo ''
 	@echo "$(Yellow)Targets:$(NC)"
 	@echo ''
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(THIS_MAKEFILE) | awk 'BEGIN {FS = ":.*?## "}; \
