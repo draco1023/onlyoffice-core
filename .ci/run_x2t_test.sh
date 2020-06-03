bc8913848405d06c77bae17b5a63ed877bee3697
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-06-03 15:00:22 +0300

    Split build and test workflow
    Add setup tests step
    
    
    Fix syntax, disable Linux to speedup builds
    
    
    Add ghostscript, fix unzip error
    
    
    Remove broken env var
    
    
    Use debug tree view for setup step
    
    
    Debug 1
    
    
    Debug 2
    
    
    Debug 3
    
    
    Debug 4
    
    
    Debug 5
    
    
    Debug 6
    
    
    Debug 7
    
    
    Debug 8

--- .ci/run_x2t_test.sh
+++ .ci/run_x2t_test.sh
@@ -3,31 +3,31 @@
 set -e
 
 BASE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
-X2T_BUILD=$BASE_PATH/X2tConverter/build/
+X2T_BUILD=$BASE_PATH/X2tConverter/build
 
 EXPECTED_JSON="$X2T_BUILD"/sample_fillable_fields_expected.txt
-ACTUAL_JSON="$X2T_BUILD"/fields_"$1".txt
+ACTUAL_JSON="$X2T_BUILD"/"$1"/result/fields_"$1".txt
 
 # Expected arguments for pdfcompare.jar: file1 file2 [image directory] [page threshold] [pixel threshold] [kernel size]
 PDFCOMPARE="java -jar pdfcompare.jar"
 PDFCOMPARE_DIR="$BASE_PATH"/.artifactory
-PDFCOMPARE_OPTS="$PDFCOMPARE_DIR/"
+PDFCOMPARE_OPTS="$X2T_BUILD"/"$1"/result/diff
 
 function extract_fields() {
 	local pdf_file="$1"
 
-	cat -v "$pdf_file" | grep "{"
+	cat -v "$pdf_file" | grep "{\"fontFamily"
 }
 
 function compare_fields() {
 	local expected="$1"
 	local actual="$2"
 
-	colordiff -s -B "$expected" "$actual"
+	diff -s -B "$expected" "$actual"
 }
 
-(>&1 echo "Searching Json fileds into PDF...")
-extract_fields "$X2T_BUILD"/"$1"/result/output.pdf > "$X2T_BUILD"/fields_"$1".txt
+(>&1 echo "Searching Json fields into PDF...")
+extract_fields "$X2T_BUILD"/"$1"/result/output.pdf > "$X2T_BUILD"/"$1"/result/fields_"$1".txt
 
 (>&1 echo "Compare reference Json file with fields from PDF...")
 compare_fields "$EXPECTED_JSON" "$ACTUAL_JSON"
